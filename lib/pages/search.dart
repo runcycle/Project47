72ee7c76c07e4a2567ab3eecdb1a4a5aadd6332f
Justin Bartholomew <jbartholomew@turinogroup.com>
2020-09-21 15:40:35 -0400

    Created search results tile

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -11,6 +12,7 @@ class Search extends StatefulWidget {
 }
 
 class _SearchState extends State<Search> {
+  TextEditingController searchController = TextEditingController();
   Future<QuerySnapshot> searchResultsFuture;
 
   handleSearch(String query) {
@@ -22,10 +24,15 @@ class _SearchState extends State<Search> {
     });
   }
 
-  buildSearchField() {
+  clearSearch() {
+    searchController.clear();
+  }
+
+  AppBar buildSearchField() {
     return AppBar(
       backgroundColor: Colors.white,
       title: TextFormField(
+        controller: searchController,
         decoration: InputDecoration(
           hintText: "Search for a user...",
           filled: true,
@@ -35,7 +42,7 @@ class _SearchState extends State<Search> {
           ),
           suffixIcon: IconButton(
             icon: Icon(Icons.clear),
-            onPressed: () => print("cleared"),
+            onPressed: clearSearch,
           ),
         ),
         onFieldSubmitted: handleSearch,
@@ -77,15 +84,16 @@ class _SearchState extends State<Search> {
           if (!snapshot.hasData) {
             return circularProgress();
           }
-          List<Text> searchResults = [];
+          List<UserResult> searchResults = [];
           snapshot.data.documents.forEach((doc) {
             User user = User.fromDocument(doc);
-            searchResults.add(Text(user.username));
+            UserResult searchResult = UserResult(user);
+            searchResults.add(searchResult);
           });
           return ListView(
             children: searchResults,
-      );
-    });
+          );
+        });
   }
 
   @override
@@ -100,8 +108,33 @@ class _SearchState extends State<Search> {
 }
 
 class UserResult extends StatelessWidget {
+  final User user;
+
+  UserResult(this.user);
+
   @override
   Widget build(BuildContext context) {
-    return Text("User Result");
+    return Container(
+      color: Theme.of(context).primaryColor.withOpacity(0.7),
+      child: Column(children: <Widget>[
+          GestureDetector(
+            onTap: () => print("Tapped"),
+            child: ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.grey,
+                backgroundImage: CachedNetworkImageProvider(user.photoUrl)
+              ),
+              title: Text(user.displayName, style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(user.username, style: TextStyle(color: Colors.white),),
+            ),
+          ),
+          Divider(
+            height: 2.0,
+            color: Colors.white54,
+          ),
+        ],
+      ),
+    );
   }
 }
