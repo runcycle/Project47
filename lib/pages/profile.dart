b4a42d2475f7da70b2a81f9c0b4f9d66c86a41e8
Justin Bartholomew <jbartholomew@turinogroup.com>
2020-09-29 16:27:21 -0400

    Grid and list view for profile page is working

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -2,11 +2,13 @@ import 'package:WatchA/models/user.dart';
 import 'package:WatchA/pages/edit_profile.dart';
 import 'package:WatchA/pages/home.dart';
 import 'package:WatchA/widgets/post.dart';
+import 'package:WatchA/widgets/post_tile.dart';
 import 'package:WatchA/widgets/progress.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:WatchA/widgets/header.dart';
+import 'package:flutter_svg/svg.dart';
 
 class Profile extends StatefulWidget {
   final String profileId;
@@ -19,6 +21,7 @@ class Profile extends StatefulWidget {
 
 class _ProfileState extends State<Profile> {
   final String currentUserId = currentUser?.id;
+  String postOrientation = "grid";
   bool isLoading = false;
   int postCount = 0;
   List<Post> posts = [];
@@ -194,8 +197,70 @@ class _ProfileState extends State<Profile> {
   buildProfilePosts() {
     if (isLoading) {
       return circularProgress();
+    } else if (posts.isEmpty) {
+      return Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SvgPicture.asset("assets/images/no_content.svg", height: 260.0),
+            Padding(
+              padding: EdgeInsets.only(top: 20.0),
+              child: Text(
+                  "No Posts Yet!",
+                  style: TextStyle(
+                    color: Colors.redAccent,
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    } else if (postOrientation == "grid") {
+      List<GridTile> gridTiles = [];
+    posts.forEach((post) {
+      gridTiles.add(GridTile(child: PostTile(post)));
+    });
+    return GridView.count(
+      crossAxisCount: 3,
+      childAspectRatio: 1.0,
+      mainAxisSpacing: 1.5,
+      crossAxisSpacing: 1.5,
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      children: gridTiles,
+    );
+    } else if (postOrientation == "list") {
+        return Column(
+        children: posts,
+
+      );
     }
-    return Column(children: posts);
+  }
+
+  setPostOrientation(String postOrientation) {
+    setState(() {
+      this.postOrientation = postOrientation;
+    });
+  }
+
+  buildTogglePostOrientation() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        IconButton(
+            onPressed: () => setPostOrientation("grid"),
+            icon: Icon(Icons.grid_on), 
+            color: postOrientation == "grid" ? Theme.of(context).primaryColor : Colors.grey,
+        ),
+        IconButton(
+          onPressed: () => setPostOrientation("list"),
+          icon: Icon(Icons.list),
+          color: postOrientation == "list" ? Theme.of(context).primaryColor : Colors.grey,
+        ),
+      ],
+    );
   }
 
   @override
@@ -205,6 +270,8 @@ class _ProfileState extends State<Profile> {
       body: ListView(
         children: <Widget>[
           buildProfileHeader(),
+          Divider(),
+          buildTogglePostOrientation(),
           Divider(
             height: 0.0,
           ),
--- lib/widgets/post_tile.dart
+++ lib/widgets/post_tile.dart
@@ -1,8 +1,17 @@
+import 'package:WatchA/widgets/custom_image.dart';
+import 'package:WatchA/widgets/post.dart';
 import 'package:flutter/material.dart';
 
 class PostTile extends StatelessWidget {
+  final Post post;
+
+  PostTile(this.post);
+
   @override
   Widget build(BuildContext context) {
-    return Text("Post Tile");
+    return GestureDetector(
+      onTap: () => print("The post was tapped"),
+      child: cachedNetworkImage(post.mediaUrl),
+    );
   }
 }
