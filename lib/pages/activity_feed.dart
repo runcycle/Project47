240901c126cea7a9f3f5a2b9e35ca50a32c395e7
Justin Bartholomew <jbartholomew@turinogroup.com>
2020-10-02 15:43:22 -0400

    Building activity feed

--- lib/pages/activity_feed.dart
+++ lib/pages/activity_feed.dart
@@ -1,3 +1,7 @@
+import 'package:WatchA/pages/home.dart';
+import 'package:WatchA/widgets/header.dart';
+import 'package:WatchA/widgets/progress.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ActivityFeed extends StatefulWidget {
@@ -6,13 +10,72 @@ class ActivityFeed extends StatefulWidget {
 }
 
 class _ActivityFeedState extends State<ActivityFeed> {
+  getActivityFeed() async {
+    QuerySnapshot snapshot = await activityFeedRef
+        .document(currentUser.id)
+        .collection("feedItems")
+        .orderBy("timestamp", descending: true)
+        .limit(50)
+        .getDocuments();
+    snapshot.documents.forEach((doc) {
+      print("Activity Feed Item: ${doc.data}");
+    });
+    return snapshot.documents;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('Activity Feed');
+    return Scaffold(
+      appBar: header(context, titleText: "Activity Feed"),
+      body: Container(
+        child: FutureBuilder(
+            future: getActivityFeed(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) {
+                return circularProgress();
+              }
+              return Text("Activity Feed");
+            }),
+      ),
+    );
   }
 }
 
+Widget mediaPreview;
+
 class ActivityFeedItem extends StatelessWidget {
+  final String username;
+  final String userId;
+  final String type; // "like" "follow" "comment"
+  final String mediaUrl;
+  final String postId;
+  final String userProfileImg;
+  final String commentData;
+  final Timestamp timestamp;
+
+  ActivityFeedItem({
+    this.username,
+    this.userId,
+    this.type, // "like" "follow" "comment"
+    this.mediaUrl,
+    this.postId,
+    this.userProfileImg,
+    this.commentData,
+    this.timestamp,
+  });
+
+  factory ActivityFeedItem.fromDocument(DocumentSnapshot doc) {
+    return ActivityFeedItem(
+      username: doc["username"],
+      userId: doc["userId"],
+      type: doc["type"],
+      postId: doc["postId"],
+      userProfileImg: doc["userProfileImg"],
+      commentData: doc["commentData"],
+      timestamp: doc["timestamp"],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Text('Activity Feed Item');
