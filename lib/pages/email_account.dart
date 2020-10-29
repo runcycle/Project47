138b4f437f7cfedc2ea83293d5b16b9f26e02f47
Justin Bartholomew <jbartholomew@turinogroup.com>
2020-10-29 14:29:55 -0400

    Working on email account submit to Firebase

--- lib/pages/email_account.dart
+++ lib/pages/email_account.dart
@@ -12,8 +12,14 @@ class EmailAccount extends StatefulWidget {
 
 class _EmailAccountState extends State<EmailAccount> {
   // final _auth = FirebaseAuth.instance;
-  // final _scaffoldKey = GlobalKey<ScaffoldState>();
-  // final _formKey = GlobalKey<FormState>();
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+  final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController _email = TextEditingController();
+  final TextEditingController _username = TextEditingController();
+  final TextEditingController _password = TextEditingController();
+  final TextEditingController _confirmPass = TextEditingController();
+
   String email;
   String username;
   String password;
@@ -26,200 +32,184 @@ class _EmailAccountState extends State<EmailAccount> {
     }
   }
 
-  passwordCompare() {
-    if (password == confirmPassword) {
-      return submit();
-    } else {
-      return null;
+  submit() {
+    final form = _formKey.currentState;
+
+    if (form.validate()) {
+      form.save();
+      SnackBar snackbar = SnackBar(content: Text("Welcome $username!"));
+      _scaffoldKey.currentState.showSnackBar(snackbar);
+      Timer(Duration(seconds: 2), () {
+        Navigator.pop(context, username);
+      });
     }
   }
-
-  submit() {}
-  // submit() {
-  //   final form = _formKey.currentState;
-  //   if (form.validate()) {
-  //     form.save();
-  //     SnackBar snackbar = SnackBar(content: Text("Welcome $username!"));
-  //     _scaffoldKey.currentState.showSnackBar(snackbar);
-  //     Timer(Duration(seconds: 2), () {
-  //       Navigator.pop(context, username);
-  //     });
-  //   }
-  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: header(context,
-          titleText: "Create Your Account", removeBackButton: false),
-      body: ModalProgressHUD(
-        inAsyncCall: _showProgress,
-        child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 24.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
+        key: _scaffoldKey,
+        backgroundColor: Colors.white,
+        appBar: header(context,
+            titleText: "Create Your Account", removeBackButton: false),
+        body: ModalProgressHUD(
+          inAsyncCall: _showProgress,
+          child: Form(
+            key: _formKey,
+            autovalidate: true,
+            child: ListView(
               children: <Widget>[
-                Text("Please Enter An Email Address",
-                    style: TextStyle(fontWeight: FontWeight.bold)),
                 SizedBox(
-                  height: 5.0,
+                  height: 20.0
                 ),
-                TextField(
+                Text("Please Enter Your Email Address", 
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                SizedBox(height: 10.0),
+                TextFormField(
+                  controller: _email,
                   keyboardType: TextInputType.emailAddress,
-                  textAlign: TextAlign.center,
-                  onChanged: (value) {
-                    email = value;
+                  validator: (val) {
+                    if (val.trim().length < 3 || val.isEmpty) {
+                      return "Email address too short";
+                    } else if (val.trim().length > 50) {
+                      return "Email address too long";
+                    } else {
+                      return null;
+                    }
                   },
+                  onSaved: (val) => email = val,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Email Address",
+                    labelStyle: TextStyle(fontSize: 15.0),
+                    hintText: "Must be at least 3 characters",
+                  ),
                 ),
-                Text("Please Enter A Username",
-                    style: TextStyle(fontWeight: FontWeight.bold)),
-                TextField(
+                SizedBox(height: 10.0),
+                Text("Please Enter A Username", 
                   textAlign: TextAlign.center,
-                  onChanged: (value) {
-                    username = value;
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                SizedBox(height: 5.0),
+                TextFormField(
+                  controller: _username,
+                  validator: (val) {
+                    if (val.trim().length < 3 || val.isEmpty) {
+                      return "Username too short";
+                    } else if (val.trim().length > 50) {
+                      return "Username too long";
+                    } else {
+                      return null;
+                    }
                   },
+                  onSaved: (val) => username = val,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Username",
+                    labelStyle: TextStyle(fontSize: 15.0),
+                    hintText: "Must be at least 3 characters",
+                  ),
                 ),
-                Text("Please Enter A Password",
-                    style: TextStyle(fontWeight: FontWeight.bold)),
-                SizedBox(
-                  height: 5.0,
+                SizedBox(height: 10.0),
+                Text("Please Enter A Password", 
+                textAlign: TextAlign.center,
+                style: TextStyle(fontWeight: FontWeight.bold),
                 ),
-                TextField(
+                SizedBox(height: 5.0),
+                TextFormField(
+                  controller: _password,
                   obscureText: true,
-                  textAlign: TextAlign.center,
-                  onChanged: (value) {
-                    password = value;
+                  validator: (val) {
+                    if (val.trim().length < 3 || val.isEmpty) {
+                      return "Password Too short";
+                    } else if (val.trim().length > 50) {
+                      return "Password Too Long";
+                    } else {
+                      return null;
+                    }
                   },
+                  onSaved: (val) => password = val,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Password",
+                    labelStyle: TextStyle(fontSize: 15.0),
+                    hintText: "Must be at least 3 characters",
+                  ),
                 ),
-                Text("Please Confirm Your Password",
-                    style: TextStyle(fontWeight: FontWeight.bold)),
-                SizedBox(
-                  height: 5.0,
+                SizedBox(height: 10.0),
+                Text("Please Confirm Your Password", 
+                textAlign: TextAlign.center,
+                style: TextStyle(fontWeight: FontWeight.bold),
                 ),
-                TextField(
+                SizedBox(height: 5.0),
+                TextFormField(
+                  controller: _confirmPass,
                   obscureText: true,
-                  textAlign: TextAlign.center,
-                  onChanged: (value) {
-                    password = value;
-              },
-            ),
-          ],
-        )),
-      ),
+                  validator: (val) {
+                    if (val != _password.text) {
+                      return "Passwords Do Not Match";
+                    } else {
+                      return null;
+                    }
+                  },
+                  onSaved: (val) => confirmPassword = val,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Password Confirmation",
+                    labelStyle: TextStyle(fontSize: 15.0),
+                    hintText: "Must be at least 3 characters",
+                ),
+              ),
+              SizedBox(height: 25.0),
+              GestureDetector(
+                  onTap: submit,
+                  child: Container(
+                    height: 50.0,
+                    width: 50.0,
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Submit",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15.0,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+            ]
+          ),
+        )   
+      )
     );
-    // return Scaffold(
-    //     key: _scaffoldKey,
-    //     appBar: header(context,
-    //         titleText: "Create Your Account", removeBackButton: true),
-    //     body: ListView(
-    //       children: <Widget>[
-    //         Container(
-    //           child: Column(
-    //             children: <Widget>[
-    //               Padding(
-    //                 padding: EdgeInsets.only(top: 25.0),
-    //                 child: Center(
-    //                   child: Text(
-    //                     "Create a Username",
-    //                     style: TextStyle(
-    //                       fontSize: 25.0,
-    //                     ),
-    //                   ),
-    //                 ),
-    //               ),
-    //               Padding(
-    //                 padding: EdgeInsets.all(16.0),
-    //                 child: Column(
-    //                   mainAxisAlignment: MainAxisAlignment.center,
-    //                   crossAxisAlignment: CrossAxisAlignment.stretch,
-    //                   children: <Widget>[
-    //                     Form(
-    //                       key: _formKey,
-    //                       autovalidate: true,
-    //                       child: Column(
-    //                         children: <Widget>[
-    //                           TextFormField(
-    //                             validator: (val) {
-    //                               if (val.trim().length < 3 || val.isEmpty) {
-    //                                 return "Username too short";
-    //                               } else if (val.trim().length > 15) {
-    //                                 return "Username too long";
-    //                               } else {
-    //                                 return null;
-    //                               }
-    //                             },
-    //                             onSaved: (val) => username = val,
-    //                             decoration: InputDecoration(
-    //                               border: OutlineInputBorder(),
-    //                               labelText: "Username",
-    //                               labelStyle: TextStyle(fontSize: 15.0),
-    //                               hintText: "Must be at least 3 characters",
-    //                               ),
-    //                             ),
-    //                             Padding(
-    //                               padding: EdgeInsets.only(top: 25.0),
-    //                               child: Center(
-    //                                 child: Text(
-    //                                   "Create a Username",
-    //                                   style: TextStyle(
-    //                                     fontSize: 25.0,
-
-    //                                   ),
-    //                                 ),
-    //                               ),
-    //                             ),
-    //                             TextFormField(
-    //                             validator: (val) {
-    //                               if (val.trim().length < 3 || val.isEmpty) {
-    //                                 return "Password too short";
-    //                               } else if (val.trim().length > 15) {
-    //                                 return "Password too long";
-    //                               } else {
-    //                                 return null;
-    //                               }
-    //                             },
-    //                             onSaved: (val) => password = val,
-    //                             decoration: InputDecoration(
-    //                               border: OutlineInputBorder(),
-    //                               labelText: "Password",
-    //                               labelStyle: TextStyle(fontSize: 15.0),
-    //                               hintText: "Must be at least 3 characters",
-    //                               ),
-    //                             ),
-    //                           ]
-    //                         )
-    //                       ),
-    //                     GestureDetector(
-    //                       onTap: submit,
-    //                       child: Container(
-    //                         height: 50.0,
-    //                         width: 100.0,
-    //                         decoration: BoxDecoration(
-    //                           color: Colors.blue,
-    //                           borderRadius: BorderRadius.circular(7.0),
-    //                         ),
-    //                         child: Center(
-    //                           child: Text(
-    //                             "Submit",
-    //                             style: TextStyle(
-    //                               color: Colors.white,
-    //                               fontSize: 15.0,
-    //                               fontWeight: FontWeight.bold,
-    //                             ),
-    //                           ),
-    //                         ),
-    //                       ),
-    //                     ),
-    //                   ]
-    //                 ),
-    //               ),
-
-    //             ],
-    //           ),
-    //         )
-    //       ],
-    //     ));
   }
 }
+
+// Form(
+//                   key: _formKey,
+//                   autovalidate: true,
+//                   child: TextFormField(
+//                     validator: (val) {
+//                       if (val.trim().length < 3 || val.isEmpty) {
+//                         return "Email address too short";
+//                       } else if (val.trim().length > 50) {
+//                         return "Email address too long";
+//                       } else {
+//                         return null;
+//                       }
+//                     },
+//                     onSaved: (val) => email = val,
+//                     decoration: InputDecoration(
+//                       border: OutlineInputBorder(),
+//                       labelText: "Email Address",
+//                       labelStyle: TextStyle(fontSize: 15.0),
+//                       hintText: "Must be at least 3 characters",
+//                     ),
+//                   ),
+//                 ),
