343993a071543fbc9b61cd77207e5267baa68bcd
Justin Bartholomew <jbartholomew@turinogroup.com>
2020-12-17 13:27:49 -0500

    Beginning to add functions that get show data from api

---
+++ lib/api/shows.dart
@@ -0,0 +1,15 @@
+import "package:WatchA/api/networking.dart";
+
+const apiKey = "974478bf";
+const omdbUrl = "http://www.omdbapi.com/";
+
+class ShowModel {
+  Future<dynamic> getShowName(String, showName) async {
+    NetworkHelper networkHelper =
+        NetworkHelper("$omdbUrl?t=$showName&apiKey=$apiKey");
+    var showData = await networkHelper.getData();
+    return showData;
+  }
+}
+
+//http://www.omdbapi.com/?i=tt3896198&apikey=974478bf
