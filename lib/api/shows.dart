c5067ee48ffa411f4ec10a5b6c2ffd4321601e27
runcycle <justinbartholomew@gmail.com>
2020-12-28 05:22:32 -0500

    Update to url in shows.dart

--- lib/api/shows.dart
+++ lib/api/shows.dart
@@ -1,5 +1,6 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:flutter/material.dart';
 //import "package:WatchA/api/networking.dart";
 import 'package:WatchA/models/show.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -29,8 +30,10 @@ class _ShowsState extends State<Shows> {
 
   //show will come from upload.dart file
 
-  Future<List<Show>> _getShow() async {
-    final response = http.get("$url?t=$show&apiKey=$apiKey");
+  Future<List<Show>> _getShow(show) async {
+    //final response = http.get("$url?t=$show&apiKey=$apiKey");
+
+    final response = await http.get("http://www.omdbapi.com/?t=$show&apikey=974478bf");
 
     if (response.statusCode == 200) {
       final result = jsonDecode(response.body);
@@ -50,3 +53,9 @@ class _ShowsState extends State<Shows> {
 
 //http://www.omdbapi.com/?i=tt3896198&apikey=974478bf
 //1 change
+
+// API Key (v3 auth)
+// 5362b48d513a9b5e2951344ceaa0c40a
+// https://api.themoviedb.org/3/movie/550?api_key=5362b48d513a9b5e2951344ceaa0c40a
+// API Read Access Token (v4 auth)
+// eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MzYyYjQ4ZDUxM2E5YjVlMjk1MTM0NGNlYWEwYzQwYSIsInN1YiI6IjVmZDI3ZThhNmM4NGQ2MDAzZjM1ZGYwNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ETyZbthZqmNGupiEBnuwj97ESQT8cs5uzq9B3Qpg3Hg
