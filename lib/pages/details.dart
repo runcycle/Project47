c756cda8ab0d8414c43d70ccf3cd856fd1a8e3f8
Justin Bartholomew <jbartholomew@turinogroup.com>
2021-01-21 16:30:16 -0500

    NestScrollView is working in details.dart

--- lib/pages/details.dart
+++ lib/pages/details.dart
@@ -14,89 +14,84 @@ class _DetailsState extends State<DetailsPage> {
   final details;
   _DetailsState(this.details);
 
+  final TextEditingController _comment = TextEditingController();
+
+  void dispose() {
+    _comment.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(details.title),
-        ),
-        body: ListView(
-          children: <Widget>[
-              Card(
-                child: Center(
-                    child: Container(
-                        height: 380,
-                        width: 255,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          image: DecorationImage(
-                            fit: BoxFit.contain,
-                            image: (details.poster) == null ? AssetImage("assets/images/noPoster.png")
-                          : NetworkImage(
-                            "https://image.tmdb.org/t/p/w500/" + details.poster),
-                                )
-                              )
-                      
+      //resizeToAvoidBottomInset: true,
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+              backgroundColor: Colors.lightBlueAccent,
+              expandedHeight: 400.0,
+              floating: false,
+              pinned: true,
+              flexibleSpace: FlexibleSpaceBar(
+                title: Text(
+                  details.title,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 15,
                   ),
                 ),
-              ),
-              SingleChildScrollView(
-                child: Card(
-                  child: Text((details.overview) == null ? "" : details.overview),
-                )
-              ),
-              SizedBox(
-              width: 150,
-              height: 30,
-              child: ElevatedButton(
-                  child: Text(
-                    "Add to Post",
-                    style: TextStyle(color: Colors.white),
+                background: Stack(children: <Widget>[
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: double.infinity,
+                    child: details.poster != null
+                        ? Image.network(
+                            "https://image.tmdb.org/t/p/w500/" + details.poster)
+                        : Image.asset("assets/images/noPoster.png"),
                   ),
-                  onPressed: () {
-
-                  },
+                  Container(
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(colors: [
+                      Colors.black.withOpacity(0.8),
+                      Colors.transparent,
+                    ])),
+                  ),
+                ]),
+              ),
+            ),
+          ];
+        },
+        body: Container(
+            width: MediaQuery.of(context).size.width,
+            height: 400,
+            padding: EdgeInsets.all(15),
+            child: ListView(children: <Widget>[
+              Text(details.overview, textAlign: TextAlign.justify),
+              SizedBox(
+                height: 20.0,
+              ),
+              //Text("Add a comment ...", style: TextStyle(color: Colors.lightBlue[700])),
+              TextField(
+                controller: _comment,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Add a comment ...",
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: RaisedButton(
+                  onPressed: () {},
+                  child:
+                      const Text("Add To Post", style: TextStyle(fontSize: 15)),
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                  elevation: 5,
                 ),
               )
-        ],
-      )
+            ])),
+      ),
     );
   }
 }
-
-// Column(
-//           children: <Widget>[
-//             Card(
-//               elevation: 5,
-//               child: Container(
-//                 height: 380,
-//                 width: 255,
-//                 decoration: BoxDecoration(
-//                   borderRadius: BorderRadius.circular(10),
-//                   image: DecorationImage(
-//                     fit: BoxFit.fitHeight,
-//                     image: (details.poster) == null ? AssetImage("assets/images/noPoster.png")
-//                   : NetworkImage(
-//                     "https://image.tmdb.org/t/p/w500/" + details.poster),
-//                   )
-//                 ),
-//               ),
-//             ),
-//             Padding(
-//               padding: const EdgeInsets.all(8.0),
-//               child: Text(details.overview),
-//             ),
-//             SizedBox(
-//               width: 200,
-//               child: ElevatedButton(
-//                   child: Text(
-//                     "Add to Post",
-//                     style: TextStyle(color: Colors.white),
-//                   ),
-//                   onPressed: () {
-
-//                   },
-//                 ),
-//             )
-//           ],
-//         )
