e4cbc5c74d30752b71b24bcc62c22948cf9af545
Justin Bartholomew <jbartholomew@turinogroup.com>
2021-04-13 16:32:01 -0400

    Test ads are working, but showing up at the wrong interval

--- lib/services/admob_service.dart
+++ lib/services/admob_service.dart
@@ -3,9 +3,9 @@ import "dart:io";
 class AdHelper {
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      return "ca-app-pub-6574097995292239~6589840728";
+      return "ca-app-pub-3940256099942544/6300978111";
     } else if (Platform.isIOS) {
-      return "ca-app-pub-6574097995292239~9350723679";
+      return "ca-app-pub-3940256099942544/2934735716";
     } else {
       throw new UnsupportedError("Unsupported platform");
     }
